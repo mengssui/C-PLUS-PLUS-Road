7287563ded82007a5c06d54ca6203277f19443df
mengssui <1711900861@qq.com>
2020-07-01 20:15:33 +0800

    特性新增: 增加了第十六章 模板与泛型编程

--- C++DataStruct/Sort/heapsort.hpp
+++ C++DataStruct/Sort/heapsort.hpp
@@ -1,18 +1,21 @@
+//堆排序，时间复杂度O(nlogn)
+
+
 #include "heap.hpp"
 
-template<typename T>
+template <typename T>
 void heapSort1(T arr[], int n) {
   MaxHeap<T> maxHeap(n);
   for (int i = 0; i < n; i++) {
     maxHeap.insert(arr[i]);
   }
-  for (int i = n-1; i >= 0; i--) {
+  for (int i = n - 1; i >= 0; i--) {
     arr[i] = maxHeap.extractMax();
   }
   return;
 }
 
-template<typename T>
+template <typename T>
 void heapSort2(T arr[], int n) {
   MaxHeap<int> maxHeap(arr, n);
   for (int i = n - 1; i >= 0; i--) {
