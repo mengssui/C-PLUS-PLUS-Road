8011350c4de2581b8b60219fe1b1a65d307223f7
mengssui <1711900861@qq.com>
2020-07-22 11:49:31 +0800

    特性新增: 增加Effective C++的笔记

---
+++ EffectiveC++3rd/learnwithpractical.cpp
@@ -0,0 +1,24 @@
+//author: Mengbw_
+//date : 2020年7月22日
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Test {
+ public:
+  enum { n = 4};  //use if not support to use static costant.
+  static const int c = 2;  //const declaration
+  int score[c];
+}; 
+
+int main() {
+  const char* const author1 = "Mengbw";
+  const string author("Mengbw");
+  Test x;
+  cout << x.score[1] << " - " << x.score[0] <<endl;
+  cout << author << '\n' << author1 << endl;
+  return 0;
+}
+
