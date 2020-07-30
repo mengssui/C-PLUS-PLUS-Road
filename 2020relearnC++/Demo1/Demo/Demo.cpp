d931508c6a0768fd3ead396a029ce742badad091
mengssui <1711900861@qq.com>
2020-07-30 21:00:44 +0800

    特性新增: 增加C++relearn的内容

--- 2020relearnC++/Demo1/Demo/Demo.cpp
+++ 2020relearnC++/Demo1/Demo/Demo.cpp
@@ -1,6 +1,6 @@
 ﻿#include "Singleton.h"
 #include "Observeralbe.h"
-
+#include "Adaptertest.h"
 using namespace std;
 class News : public Observeralbe{
  public: 
@@ -12,27 +12,38 @@ int main() {
   //单例模式
   //Singleton::getInstance()->DoSomething();
   
-  //观察者模式
-  User1 u1;
-  User2 u2;
+  ////观察者模式
+  //User1 u1;
+  //User2 u2;
 
-  News n1;
-  n1.GetSomeNews("T0");
-  cout << n1.GetObserverCnt() << endl;//0
+  //News n1;
+  //n1.GetSomeNews("T0");
+  //cout << n1.GetObserverCnt() << endl;//0
 
-  n1.Attach(&u1);
-  n1.Attach(&u2);
-  n1.GetSomeNews("T1");
-  cout << n1.GetObserverCnt() << endl;//0
+  //n1.Attach(&u1);
+  //n1.Attach(&u2);
+  //n1.GetSomeNews("T1");
+  //cout << n1.GetObserverCnt() << endl;//0
 
-  n1.Detach(&u2);
-  n1.GetSomeNews("T2");
-  cout << n1.GetObserverCnt() << endl;//1
+  //n1.Detach(&u2);
+  //n1.GetSomeNews("T2");
+  //cout << n1.GetObserverCnt() << endl;//1
 
-  n1.DetachAll();
-  n1.GetSomeNews("T3");
-  cout << n1.GetObserverCnt() << endl;
+  //n1.DetachAll();
+  //n1.GetSomeNews("T3");
+  //cout << n1.GetObserverCnt() << endl;
 
-  cout << "exit main function"<< endl;
+  //cout << "exit main function"<< endl;
+
+  //适配器模式
+  double x = 20.0, y = 50.0, w = 300.0, h =200.0;
+  RectangleAdapter ra(x,y,w,h);
+  Rectangle* pR = &ra;
+  pR->Draw("Testing Adapter");
+
+  
+  RectangleAdapter2 ra2(x,y,w,h);
+  Rectangle* pR2 = &ra2;
+  pR2->Draw("Testing2 Adapter");
   return 0;
 }
