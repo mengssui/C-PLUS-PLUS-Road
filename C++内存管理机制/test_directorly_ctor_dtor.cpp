2fdf64d932868c0ab9a91a60ae93d24cc3ebea91
mengsansui <1711900861@qq.com>
2021-06-07 23:44:57 +0800

    侯捷内存管理: 理解new的过程，测试是否能够直接通过指针调用构造函数和析构函数

---
+++ C++内存管理机制/test_directorly_ctor_dtor.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+//#include <memory>				 //std::allocator  
+using namespace std;
+
+namespace jj02 {
+
+class A {
+public:
+  A() : id(0)      { cout << "default ctor. this="  << this << " id=" << id << endl;  }
+  A(int i) : id(i) { cout << "ctor. this="  << this << " id=" << id << endl;  }
+  ~A()             { cout << "dtor. this="  << this << " id=" << id << endl;  }
+  int id;
+};
+	
+void test_call_ctor_directly() {
+	cout << "\ntest_call_ctor_directly().......... \n";	
+	
+  string* pstr = new string;
+  cout << "str= " << *pstr << endl;
+    
+//! pstr->string::string("jjhou");  
+                        //[Error] 'class std::basic_string<char>' has no member named 'string'
+//! pstr->~string();	//crash -- 其語法語意都是正確的, crash 只因為上一行被 remark 起來嘛.  
+  cout << "str= " << *pstr << endl;
+
+
+//------------
+
+  	A* pA = new A(1);         	//ctor. this=000307A8 id=1
+  	cout << pA->id << endl;   	//1
+//!	pA->A::A(3);                //in VC6 : ctor. this=000307A8 id=3
+  								//in GCC : [Error] cannot call constructor 'jj02::A::A' directly
+                  //[Error] in my enviroment GCC @mengbw
+  								
+//!	A::A(5);	  				//in VC6 : ctor. this=0013FF60 id=5
+                      			//         dtor. this=0013FF60  	
+  								//in GCC : [Error] cannot call constructor 'jj02::A::A' directly
+  								//         [Note] for a function-style cast, remove the redundant '::A'
+		
+  	cout << pA->id << endl;   	//in VC6 : 3
+  								//in GCC : 1  	
+  	
+  	delete pA;                	//dtor. this=000307A8 
+
+  	//simulate new 模拟编译器执行new的过程
+  	void* p = ::operator new(sizeof(A));  
+  	cout << "p=" << p << endl; 	//p=000307A8
+  	pA = static_cast<A*>(p);
+//!	pA->A::A(2);				//in VC6 : ctor. this=000307A8 id=2
+  								//in GCC : [Error] cannot call constructor 'jj02::A::A' directly  	
+    new(pA) A(3);	//@mengbw: 使用原地构造的方式
+  	cout << pA->id << endl;     //in VC6 : 2
+  								//in GCC : 0  	
+
+  	//simulate delete
+  	pA->~A();					//dtor. this=000307A8 
+  	::operator delete(pA);		//free()
+}
+}
+
+int main() {
+
+  jj02::test_call_ctor_directly();
+  return 0;
+}
